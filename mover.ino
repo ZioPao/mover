0bea922b4d3159fd754ad9b104e6dafd2b5f0fe8
ZioPao <picchetti1998@gmail.com>
2021-05-04 20:37:18 +0200

    added software handshake + reset via sw

--- mover.ino
+++ mover.ino
@@ -1,77 +1,137 @@
 #include "header.h"
 
 //Setup variables
-bool isConnectionEstabilished;
-
-//Timer stuff
-MiniTimer timerPrinting;
-MiniTimer timerMovement;
-
-//Acceleration variables
-int16_t fMov, sMov;
+bool isManagerConnectionEstabilished;
+bool isBluetoothConnectionEstabilished;
 
 //Components
 XinputMovement xinputMovement;
 BluetoothLink bluetoothLink;
 IMUManager imuManager;
 
+//Timer stuff
+MiniTimer timerPrinting;
+MiniTimer timerMovement;
+
+//Movement variables
+VectorInt16 mAcc, sAcc;
+VectorInt16 mGyr, sGyr;
+
 void setup()
 {
 
+  while (!Serial)
+    ; // wait for Leonardo enumeration, others continue immediately
+    
+  delay(3000);
 
-  //TEST
-  pinMode(17, OUTPUT);
+  Serial.begin(9600); //not sure if it's correct
+  Serial.println("MASTER");
 
+  //Await Serial connection... todo handshake
+  isManagerConnectionEstabilished = false;
+  isBluetoothConnectionEstabilished = false;
 
-  isConnectionEstabilished = false;
-  imuManager.setup();
-  fMov = 0;
-  sMov = 0;
-
-  //Timer setup
-  timerPrinting.setup(TIMER_PRINTING);
-  timerMovement.setup(TIMER_MOVEMENT);
-
+  while (!isManagerConnectionEstabilished)
+  {
+    char chk_connection = Serial.read();
+    isManagerConnectionEstabilished = (chk_connection == 'c');
+  }
+  
 #ifdef DISABLE_BT_TEST
   isConnectionEstabilished = true;
 #endif
 
   //BT test connection
-  while (!isConnectionEstabilished)
+  while (!isBluetoothConnectionEstabilished)
   {
-    isConnectionEstabilished = bluetoothLink.checkConnectionMaster();
+    isBluetoothConnectionEstabilished = bluetoothLink.checkConnectionMaster();
   }
+#ifdef ENABLE_IMU
+  imuManager.setup();
+#endif
+
+  //Timer setup
+  timerPrinting.setup(TIMER_PRINTING);
+  timerMovement.setup(TIMER_MOVEMENT);
 }
 
-/////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
 void printValues()
 {
-  Serial.print("Main value -> ");
-  Serial.print(fMov);
 
-  Serial.print("   ");
+  /* VALUES FROM MAIN MOVER*/
+  Serial.print("main");
+  Serial.println();
 
-  Serial.print("Value from slave -> ");
-  Serial.print(sMov);
+  //Accelerometer
+  Serial.print(mAcc.x);
+  Serial.print(",");
+  Serial.print(mAcc.y);
+  Serial.print(",");
+  Serial.print(mAcc.z);
+  Serial.print(",");
+
+  //Gyroscope
+  Serial.print(mGyr.x);
+  Serial.print(",");
+  Serial.print(mGyr.y);
+  Serial.print(",");
+  Serial.print(mGyr.z);
+
+  /* VALUES FROM SLAVE MOVER*/
+  Serial.println();
+  Serial.print("slave");
+  Serial.println();
+
+  //Accelerometer
+  Serial.print(sAcc.x);
+  Serial.print(",");
+  Serial.print(sAcc.y);
+  Serial.print(",");
+  Serial.print(sAcc.z);
+  Serial.print(",");
+
+  //Gyroscope
+  Serial.print(sGyr.x);
+  Serial.print(",");
+  Serial.print(sGyr.y);
+  Serial.print(",");
+  Serial.print(sGyr.z);
 
   Serial.println();
 }
-////////////////////////////////////////
+
+void checkEvents()
+{
+
+  char cmd = Serial.read();
+
+  switch (cmd)
+  {
+  case 'r':
+
+    //rerun setup
+    setup();
+    break;
+  }
+}
+//////////////////////////////////////////////////////////////////////////////////
 
 void loop()
 {
 
-  //Gets the necessary values
-  //fMov = imuManager.getRealAcceleration();
-  fMov = imuManager.getGyroStatus();
-  //sMov = bluetoothLink.getData();
+  imuManager.getGyroAndAccelValues(&mAcc, &mGyr);
+  bluetoothLink.getData(&sAcc, &sGyr);
 
-  //Printing and debug
+  // Printing and debug
   if (timerPrinting.update())
   {
     printValues();
   }
 
-    //xinputMovement.manageMotions(fMov, sMov);
-  
+  //Event handling from outer inputs like the manager
+  checkEvents();
+
+  //xinputMovement.manageMotions(fMov, sMov);
 }
