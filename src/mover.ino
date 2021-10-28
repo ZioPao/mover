5ff359f3378d15b3efa9bd1d3313b580f7bd9125
ZioPao <picchetti1998@gmail.com>
2021-10-28 14:03:03 +0200

    modified the whole master slave stuff

--- src/mover.ino
+++ src/mover.ino
@@ -1,6 +1,6 @@
 #include "header.h"
 
-#define MASTER
+//#define LEFT
 
 //Setup variables
 bool isManagerConnectionEstabilished;
@@ -47,10 +47,10 @@ void setup()
 
   Serial.begin(115200);
 
-#ifdef MASTER
-  Serial.println("MASTER");
+#ifdef LEFT
+  Serial.println("LEFT");
 #else
-  Serial.println("SLAVE");
+  Serial.println("RIGHT");
 #endif
   delay(100); //Await Serial connection... todo handshake
 
@@ -82,9 +82,9 @@ void setup()
 
   // Bluetooth setup
 
-  Serial1.begin(115200);
-  bluetooth_transfer.begin(Serial1);
-
+  //Serial1.begin(115200);
+  //bluetooth_transfer.begin(Serial1, true);
+  
   // Debug LEDs setup
 
   pinMode(DEBUG_LED_PIN, OUTPUT);
@@ -131,7 +131,7 @@ void checkEvents()
   }
 }
 
-#ifdef MASTER
+
 //////////////////////////////////////////////////////////////////////////////////
 // MASTER ONLY
 //////////////////////////////////////////////////////////////////////////////////
@@ -144,120 +144,44 @@ void printValues()
 
   /// X FILTERING
 
-  float b[] = {0.00066048, 0.00132097, 0.00066048};
-  float a[] = {1.92600086, -0.92864279};
-  
+  //float b[] = {0.00066048, 0.00132097, 0.00066048};
+  //float a[] = {1.92600086, -0.92864279};
 
-  float x_filter = 0.9601172921934632 * x_filter_old + 0.019941353903268455 * acc.x + 0.019941353903268566 * x_og_old;
-  x_filter_old = x_filter;
-  x_og_old = acc.x;
+
+  //float x_filter = 0.9601172921934632 * x_filter_old + 0.019941353903268455 * acc.x + 0.019941353903268566 * x_og_old;
+  //x_filter_old = x_filter;
+  //x_og_old = acc.x;
 
 
   // Y FILTERING
-  float y_filter = 0.9601172921934632 * y_filter_old + 0.019941353903268455 * acc.y + 0.019941353903268566 * y_og_old;
-  y_filter_old = y_filter;
-  y_og_old = acc.y;
+  //float y_filter = 0.9601172921934632 * y_filter_old + 0.019941353903268455 * acc.y + 0.019941353903268566 * y_og_old;
+  //y_filter_old = y_filter;
+  //y_og_old = acc.y;
 
 
   // Z FILTERING
-  float z_filter = 0.9601172921934632 * z_filter_old + 0.019941353903268455 * acc.z + 0.019941353903268566 * z_og_old;
-  z_filter_old = z_filter;
-  z_og_old = acc.z;
+  //float z_filter = 0.9601172921934632 * z_filter_old + 0.019941353903268455 * acc.z + 0.019941353903268566 * z_og_old;
+  //z_filter_old = z_filter;
+  //z_og_old = acc.z;
 
 
   // TEST ONLY ACCELEROMETERS
-
-  String acc_string = String(acc.x);    // unfiltered x
-  acc_string += ",";
-  acc_string += String(y_filter);
-  acc_string += ",";
-  acc_string += String(z_filter);
-  acc_string += ",";
-  acc_string += String(millis());
-  Serial.println(acc_string);
-  /*
-
-  String acc_string = "a,";
-  acc_string += String(acc.x);
-  acc_string += ",";
-  acc_string += String(acc.y);
-  acc_string += ",";
-  acc_string += String(acc.z);
-  //acc_string += ",";
-  //acc_string += String(second_acc.x);
-  //acc_string += ",";
-  //acc_string += String(second_acc.y);
-  //acc_string += ",";
-  //acc_string += String(second_acc.z);
-
-  Serial.println(acc_string);
-
-  String gyr_string = "g,";
-
-  gyr_string += String(gyr.y);
-  gyr_string += ",";
-  gyr_string += String(gyr.z);
-  gyr_string += ",";
-  //gyr_string += String(second_gyr.y);
-  //gyr_string += ",";
-  //gyr_string += String(second_gyr.z);
-  //gyr_string += ",";
-
-  gyr_string += String(millis());
-
-  Serial.println(gyr_string);
-*/
-}
-
-void receiveData(VectorInt16* a, Vector2Float* g)
-{
-
-  #ifndef DEBUG_MASTER
-  uint16_t received_data = 0;
-  float packet[5];
-
-  if (bluetooth_transfer.available())
-  {
-    //bluetooth_transfer.rxObj(second_acc, received_data);
-    bluetooth_transfer.rxObj(packet, received_data);
-
-    a -> x = packet[0];
-    a -> y  = packet[1];
-    a -> z  = packet[2];
-    g -> y  = packet[3];
-    g -> z  = packet[4];
-
-  }
+  #ifdef LEFT
+  String printed_string = "l,";
+  #else
+  String printed_string = "r,";
   #endif
-}
-#else
-//////////////////////////////////////////////////////////////////////////////////
-// SLAVE ONLY
-//////////////////////////////////////////////////////////////////////////////////
-void sendData(){
-  
-
-  //we pack it in a single array 
-
-  float packet[5];
-
-  packet[0] = acc.x;
-  packet[1] = acc.y;
-  packet[2] = acc.z;
-  packet[3] = gyr.y;
-  packet[4] = gyr.z;
-
-  uint16_t sent_data = 0;
-  sent_data = bluetooth_transfer.txObj(packet, sent_data); 
-  bluetooth_transfer.sendData(sent_data);
-  digitalWrite(DEBUG_LED_PIN, HIGH);
-  delay(100);
-  digitalWrite(DEBUG_LED_PIN, LOW);
+  printed_string += String(acc.x);    
+  printed_string += ",";
+  printed_string += String(acc.y);
+  printed_string += ",";
+  printed_string += String(acc.z);
+  printed_string += ",";
+  printed_string += String(millis());
+  Serial.println(printed_string);
 
 }
 
-
-#endif
 
 //////////////////////////////////////////////////////////////////////////////////
 
@@ -266,16 +190,14 @@ void loop()
 
   imuManager.updateValues();
   acc = imuManager.getAcceleration();
-  gyr = imuManager.getPitchRoll();
+  //gyr = imuManager.getPitchRoll();
 
-#ifdef MASTER
-  receiveData(&second_acc, &second_gyr);
+  //receiveData(&second_acc);
   printValues();
 
-#else
-  sendData();
 
-#endif
+  //sendData();
+
 
   checkEvents();
 
